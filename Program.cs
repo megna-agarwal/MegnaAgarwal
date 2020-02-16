1a583dc75ae3231bdb4eaa7ce7f0fc48d9bdd95f
Meghna <meghna@example.com>
2020-02-16 08:43:43 +0545

    database  changes

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Megna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console .WriteLine("Megna Agarwal");
+            int x=10;
+            int y=20;
+            int z= x+y;
+            Console.WriteLine(z);
+        }
+    }
+}
